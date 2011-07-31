112cf8448087132a00938c9d416a9c2d2d7d1073
Jose Diaz <jocddz@gmail.com>
2011-07-30 23:48:37 -0430

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+# Actualizando Gemfile con las gemas que necesita esta Aplicacion
+# La gema RSpec y la gema de la biblioteca RSpec especificas para Rails
+
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+gem 'sqlite3', '1.3.3'
+
+group :development do
+	gem 'rspec-rails', '2.6.1'
+end
+
+group :test do
+	gem 'rspec-rails', '2.6.1'
+	gem 'webrat', '0.7.3'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppEstatica::Application.config.secret_token = '242a227e04d6f1cb75989eea6a72ed2c38a24473ea40ec19e1580c19c34f5d3cf28b5b464e56c86909d2cead6ef97ac759ac0bb8156f61ee7c28c9a462a1615b'
